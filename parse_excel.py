9f6933572f72a87425f67f369b8ebebdc22ab17d
Adam-205 <a205@umbc.edu>
2020-04-10 13:22:44 -0400

    Added function that can load classrooms from the .xlsx file we were given

--- parse_excel.py
+++ parse_excel.py
@@ -2,9 +2,10 @@
 from openpyxl import load_workbook
 
 #Constants
-FILENAME = "Spring 2020 Schedule.xlsx" #Set this to the path of any excel spreadsheet
+schedule = "Spring 2020 Schedule.xlsx" #Set this to the path of any excel spreadsheet
+classroom = "ClassRoom.xlsx"
 
-#These are for the partial excel sheet
+#These are for the partial excel sheet - specifically for the schedule excel sheet
 SUBJECT = 1
 COURSE_NUM = 2
 COURSE_TITLE = 3
@@ -23,6 +24,8 @@ output: a list containing all data entries for a given column
 Description: 
 Given the path for an excel spreadsheet and a column number,
 load all entries from that column and return them as a list.
+
+Note: This works with both spreadsheets but doesn't strictly define datatypes
 """
 def load_excel_spreadsheet_partial(filename, column):
 	data = []
@@ -47,7 +50,7 @@ def load_excel_spreadsheet_partial(filename, column):
 	
 	return data
 
-capacities = load_excel_spreadsheet_partial(FILENAME, CAPACITY)
+capacities = load_excel_spreadsheet_partial(schedule, CAPACITY)
 print(capacities) #This was used to verify results
 
 
@@ -65,7 +68,7 @@ Note: This is the format of the columns from excel sheet
 ['Subject', 'Course #', 'Course Title', 'Ver.', 'Sec.', 'Instructor Real Name', 'Time', 'Capacity']
 
 """
-def load_excel_spreadsheet(filename):
+def load_schedule(filename):
 	#Either of these work, it's a matter of preference
 	subjects, course_nums, course_titles, versions, sections, instructor_real_names, times, capacities = [], [], [], [], [], [], [], []
 
@@ -126,6 +129,52 @@ def load_excel_spreadsheet(filename):
 			
 	return subjects, course_nums, course_titles, versions, sections, instructor_real_names, times, capacities
 
-subjects, course_nums, course_titles, versions, sections, instructor_real_names, times, capacities = load_excel_spreadsheet(FILENAME)
+subjects, course_nums, course_titles, versions, sections, instructor_real_names, times, capacities = load_schedule(schedule)
 
 print(subjects) #This was used to verify results
+
+
+
+"""
+These functions are similar to the load schedule functions except that they are for the class room files
+"""
+def load_classrooms(filename):
+	#Either of these work, it's a matter of preference
+	classrooms, capacities = [], []
+	
+	"""
+	classroom = []
+	capacity = []
+	"""
+	workbook = load_workbook(filename) 		#Load .xlsx file
+	sheet = workbook.active				#Current excel sheet (we are only using one)
+	rows = sheet.rows				#Rows from excel sheet
+	max_row = sheet.max_row				#Gets count of max row
+	
+	"""
+	Why start the loop at 2:
+		We do not need the first row which contains the headers i.e. subject, course #, etc.
+		Additionally, iterating through an excel spreadsheet starts at 1 not 0.		
+		
+	Why end the loop at max_row + 1:
+		Since the iteration starts at one, we need to add +1 at the end to get every row
+	"""
+	for i in range(2, max_row + 1): #do not need first row
+		
+		"""
+		According to Dutt the excel sheet we will be given will always be the same format
+		This is hardcoded based on that format, but it is easy to change as needed.
+		"""
+		classroom = sheet.cell(row=i, column=1)
+		classrooms.append(classroom.value)
+		
+		capacity = sheet.cell(row=i, column=2) 
+		capacities.append(capacity.value)
+	
+	return classrooms, capacities
+
+
+classrooms, capacities = load_classrooms(classroom)
+print(classrooms) #This was used to verify results
+
+
