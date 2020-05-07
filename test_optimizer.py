ee7238ec42a456c195ff713b8a168b5377ef11a9
Dyl258 <43890292+Dyl258@users.noreply.github.com>
2020-05-07 14:49:05 -0400

    Update test_optimizer.py

--- test_optimizer.py
+++ test_optimizer.py
@@ -4,21 +4,21 @@ test1 = [1, 2, 3]
 test2 = [1, 2, 3]
 
 def capacity_constraint(classs, room):
-    if test1[classs]  <= test2[room]:
+    if classes[classs]  <= rooms[room]:
         return True
     return False
 
-def optimizer(classes, rooms, num_courses, num_classrooms):
+def optimizer(num_courses, num_classrooms):
     problem = Problem()
-    problem.addVariable("c", range(len(test1)))
-    problem.addVariable("r", range(len(test2)))
+    problem.addVariable("c", range(num_courses))
+    problem.addVariable("r", range(num_classrooms))
     problem.addConstraint(capacity_constraint, ['c', 'r'])
     solutions = problem.getSolutions()
     print(solutions)
 
     for i in range(len(solutions)):
-        #print(solutions[i]["c"].title, end=" in ")
-        #print(solutions[i]["r"].room)
-        print("Test1 at index", solutions[i]["c"], " Test2 at index", solutions[i]["r"])
+        print(solutions[i]["c"].title, end=" in ")
+        print(solutions[i]["r"].room)
+        #print("Test1 at index", solutions[i]["c"], " Test2 at index", solutions[i]["r"])
 
     print(len(solutions))
